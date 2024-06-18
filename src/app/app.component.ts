31879c4a62eeff34b5280b20804f16a849e25155
Evaldas Pocius <evaldukas96@gmail.com>
2024-06-18 17:46:36 +0300

    initial introduction component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './sections/hero/hero.component';
+import { IntroductionComponent } from './sections/introduction/introduction.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroComponent],
+  imports: [RouterOutlet, HeroComponent, IntroductionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
