4731aa84090f9289d21f99e1a645770b66fd2cd7
Evaldas Pocius <evaldukas96@gmail.com>
2024-06-20 08:17:34 +0300

    display tech companies

--- src/app/sections/tech/tech.component.ts
+++ src/app/sections/tech/tech.component.ts
@@ -5,8 +5,13 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './tech.component.html',
-  styleUrl: './tech.component.scss'
+  styleUrl: './tech.component.scss',
 })
 export class TechComponent {
-
+  public readonly componies = [
+    'tech/unreal.png',
+    'tech/unity.png',
+    'tech/oculus.png',
+    'tech/vive.png',
+  ];
 }
