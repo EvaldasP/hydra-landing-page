c458b5cdc47ce5f5867c2e9fa56243135a7ba7d0
Evaldas Pocius <evaldukas96@gmail.com>
2024-06-30 17:56:44 +0300

    navigation mobile view

--- src/app/sections/hero/components/navigation/navigation.component.ts
+++ src/app/sections/hero/components/navigation/navigation.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
 })
 export class NavigationComponent {
   public readonly navLinks = ['About', 'Services', 'Technologies', 'How To'];
+
+  dropdownOpen = false;
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleOutsideClick(event: Event) {
+    if (!event.target || !(event.target as HTMLElement).closest('.relative')) {
+      this.dropdownOpen = false;
+    }
+  }
 }
