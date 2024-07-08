7056b94d5e01f5f1a7e76341f04fa94021f76532
Evaldas Pocius <evaldukas96@gmail.com>
2024-07-08 22:47:57 +0300

    try to optimaze image load

--- src/app/sections/hero/hero.component.ts
+++ src/app/sections/hero/hero.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { HeroContactsComponent } from './components/hero-contacts/hero-contacts.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [NavigationComponent, HeroContactsComponent],
+  imports: [NavigationComponent, HeroContactsComponent, NgOptimizedImage],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
